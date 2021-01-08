197317dcc20949397577e12fefb70277cc3f64a2
WinstonMoh <chepson4@gmail.com>
2021-01-07 18:13:03 -0600

    Cocktail grids done

--- lib/main.dart
+++ lib/main.dart
@@ -73,13 +73,56 @@ final Map<String, String> _discoverImages = {
       'Sparkling',
 };
 
+final Map<String, String> _latestCocktails = {
+  'https://vignette.wikia.nocookie.net/cocktails/images/0/04/Mikado.jpg/revision/latest/scale-to-width-down/340?cb=20150717021317':
+      'Mikado',
+  'https://i.pinimg.com/originals/a4/06/60/a40660adce58e8ed806b77f21844a23c.jpg':
+      'Olympic',
+  'https://www.thespruceeats.com/thmb/CwuMEo5bx7Q6kXOwV3tmHgjgQwg=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/gin-rickey-recipe-760083-hero-01-67f0e60bf4594e1893982c1dad67c548.jpg':
+      'Gin Gin Highball',
+  'https://www.saveur.com/resizer/zLMzuR56gVbxkG78D3tamPdAZyY=/760x1140/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/NAJERTA42U3MVMST6YN76GYDDY.jpg':
+      'Upside-down Martini',
+};
+
+final Map<String, String> _iconicCocktails = {
+  'http://www.allwhitebackground.com/images/5/Cocktail-Background-White-Image.png':
+      'Surprise Me!',
+  'https://www.goslingsrum.com/wp-content/uploads/2015/06/darknstormy-full.jpg':
+      'Dark and Stormy',
+  'https://makemeacocktail.com/images/cocktails/8433/2014743684d558cf0b0d311a10b707300e51.jpg':
+      'Daiquiri',
+  'https://www.cocktail-db.com/stat/img/1280/PinaColada.jpg': 'Pina Colada'
+};
+
+final Map<String, String> _aroundTheWorld = {
+  'https://thumbs.dreamstime.com/b/cocktail-served-lemon-slice-flag-france-french-104139735.jpg':
+      'France',
+  'https://thumbs.dreamstime.com/b/cocktail-served-lemon-slice-flag-italy-italian-104140026.jpg':
+      'Italy',
+  'https://thumbs.dreamstime.com/b/american-cocktail-served-lemon-slice-flag-united-states-america-103949980.jpg':
+      'USA',
+  'https://thumbs.dreamstime.com/b/english-cocktail-cocktail-served-lemon-slice-flag-united-kingdom-104367574.jpg':
+      'United Kingdom'
+};
+
+final Map<String, String> _cocktailType = {
+  'https://www.saveur.com/resizer/3frWHxg9pbYmXJ7bmveRIDSrenA=/869x652/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/EVQX6TRB4MWFFKUMQKFVCCQICI.jpg':
+      'Classic',
+  'https://cdn.trendhunterstatic.com/thumbs/camel-milk.jpeg': 'Creamy',
+  'https://previews.123rf.com/images/dml5050/dml50501203/dml5050120300091/12932289-alcohol-margarita-cocktails-or-long-island-iced-tea-with-lime-in-short-cocktail-glasses-isolated-on-.jpg':
+      'Short',
+  'https://lh3.googleusercontent.com/proxy/Vtyb9UK3NPKBZpbtO1ipffOz66UYVZyf1oB5e7PF3jSP-gJw6FumR9LA_GgzNnGepc25qqB2XMve0VtkMi_PUf6FCICKVRpIHBzMLE0C_lzdLVIYTxMCB8QwYc4G':
+      'Longdrink'
+};
+
 List<Widget> _discoverSection = <Widget>[
   Container(
+    padding: EdgeInsets.all(20),
     child: Text(
       'Discover',
       style: TextStyle(
         fontWeight: FontWeight.bold,
-        fontSize: 30,
+        fontSize: 40,
       ),
     ),
   ),
@@ -91,13 +134,12 @@ List<Widget> _discoverSection = <Widget>[
                 Center(
                     child: Image.network(
                   item.key,
-                  fit: BoxFit.cover,
+                  fit: BoxFit.fill,
                   width: 500,
                   height: 200,
                 )),
                 Text(item.value,
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
                       fontSize: 20,
                     )),
               ]))
@@ -105,48 +147,143 @@ List<Widget> _discoverSection = <Widget>[
     ),
   ),
   Container(
+    padding: EdgeInsets.all(20),
     child: Text(
       'Iconic Cocktails',
       style: TextStyle(
         fontWeight: FontWeight.normal,
-        fontSize: 30,
+        fontSize: 20,
       ),
     ),
   ),
   Container(
-    child: Text(
-      'Base Spirits',
-      style: TextStyle(
-        fontWeight: FontWeight.normal,
-        fontSize: 30,
-      ),
+    child: GridView.count(
+      crossAxisCount: 2,
+      crossAxisSpacing: 0.5,
+      mainAxisSpacing: 0.5,
+      shrinkWrap: true,
+      physics: ScrollPhysics(),
+      children: _iconicCocktails.entries
+          .map((item) => Column(children: <Widget>[
+                Text(item.value,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    )),
+                Flexible(
+                    child: Image.network(
+                  item.key,
+                  fit: BoxFit.fill,
+                  width: 180,
+                  height: 180,
+                )),
+              ]))
+          .toList(),
     ),
   ),
   Container(
+    padding: EdgeInsets.all(20),
     child: Text(
       'Cocktail Type',
       style: TextStyle(
         fontWeight: FontWeight.normal,
-        fontSize: 30,
+        fontSize: 20,
       ),
     ),
   ),
   Container(
+    child: GridView.count(
+      crossAxisCount: 2,
+      crossAxisSpacing: 0.5,
+      mainAxisSpacing: 0.5,
+      shrinkWrap: true,
+      physics: ScrollPhysics(),
+      children: _cocktailType.entries
+          .map((item) => Column(children: <Widget>[
+                Flexible(
+                    child: Image.network(
+                  item.key,
+                  fit: BoxFit.fill,
+                  width: 180,
+                  height: 180,
+                )),
+                Text(item.value,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    )),
+              ]))
+          .toList(),
+    ),
+  ),
+  Container(
+    padding: EdgeInsets.all(20),
     child: Text(
       'Latest Cocktails',
       style: TextStyle(
         fontWeight: FontWeight.normal,
-        fontSize: 30,
+        fontSize: 20,
       ),
     ),
   ),
   Container(
+    child: GridView.count(
+      crossAxisCount: 2,
+      crossAxisSpacing: 0.5,
+      mainAxisSpacing: 0.5,
+      shrinkWrap: true,
+      physics: ScrollPhysics(),
+      children: _latestCocktails.entries
+          .map((item) => Column(children: <Widget>[
+                Text(item.value,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    )),
+                Flexible(
+                    child: Image.network(
+                  item.key,
+                  fit: BoxFit.cover,
+                  width: 180,
+                  height: 180,
+                )),
+              ]))
+          .toList(),
+    ),
+  ),
+  Container(
+    padding: EdgeInsets.all(20),
     child: Text(
       'Around the World',
       style: TextStyle(
         fontWeight: FontWeight.normal,
-        fontSize: 30,
+        fontSize: 20,
       ),
+    ),
+  ),
+  Container(
+    child: GridView.count(
+      crossAxisCount: 2,
+      crossAxisSpacing: 0.5,
+      mainAxisSpacing: 0.5,
+      shrinkWrap: true,
+      physics: ScrollPhysics(),
+      children: _aroundTheWorld.entries
+          .map((item) => Column(children: <Widget>[
+                Flexible(
+                    child: Image.network(
+                  item.key,
+                  fit: BoxFit.cover,
+                  width: 180,
+                  height: 180,
+                )),
+                Text(item.value,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    )),
+              ]))
+          .toList(),
     ),
   ),
 ];
