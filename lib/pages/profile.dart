ce61a9d23b6eb90a6b1423e7d3d45f80d5f61c6f
WinstonMoh <chepson4@gmail.com>
2021-05-04 17:19:16 -0400

    [auth] clean code

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -10,56 +10,54 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+        body: ListView(children: <Widget>[
+      Container(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Icon(Icons.chevron_left_rounded)),
           Container(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(Icons.chevron_left_rounded)),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  'Profile',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Raleway',
-                    fontSize: 40,
-                  ),
-                ),
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Raleway',
+                fontSize: 40,
               ),
-              Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.blue, width: 4.0),
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: NetworkImage(picture ?? ''),
-                  ),
-                ),
+            ),
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue, width: 4.0),
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(picture ?? ''),
               ),
-              SizedBox(height: 24.0),
-              Text('Email address\n$email'),
-              SizedBox(height: 48.0),
-              ElevatedButton(
-                onPressed: () {
-                  logoutAction();
-                  Navigator.pop(context);
-                },
-                child: Text('Logout'),
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateColor.resolveWith((states) => Colors.red)),
-              ),
-            ],
-          ))
-        ]));
+            ),
+          ),
+          SizedBox(height: 24.0),
+          Text('Email address\n$email'),
+          SizedBox(height: 48.0),
+          ElevatedButton(
+            onPressed: () {
+              logoutAction();
+              Navigator.pop(context);
+            },
+            child: Text('Logout'),
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateColor.resolveWith((states) => Colors.red)),
+          ),
+        ],
+      ))
+    ]));
   }
 }
